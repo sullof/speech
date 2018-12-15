d905b150d979f0b6d6322c863e2e8d0c39f500aa
Francesco Sullo <sullof@gmail.com>
2018-12-14 22:13:31 -0800

    Minor change

--- bin/run.sh
+++ bin/run.sh
@@ -3,9 +3,9 @@
 docker rm -f dev-tools-slides
 
 docker run \
-  --name tron-dev-tools-slides \
+  --name dev-tools-slides \
   -p 9876 \
   -e VIRTUAL_HOST=sullo.co,www.sullo.co,tron-dev-tools-slides.sullo.co \
   -e LETSENCRYPT_HOST=sullo.co,www.sullo.co,tron-dev-tools-slides.sullo.co \
   -e LETSENCRYPT_EMAIL=admin@sullo.co \
-  -d dev-tools-slides
+  -d tron-dev-tools-slides
